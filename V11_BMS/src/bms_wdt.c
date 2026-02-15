0ab1e86b62884d55900f5563454f060cdad58efe
Vladislav Gyurov <vladislav.gyurov@gmail.com>
2026-02-15 12:45:31 +0200

    wdt period increased

--- V11_BMS/src/bms_wdt.c
+++ V11_BMS/src/bms_wdt.c
@@ -56,8 +56,8 @@ void bms_wdt_init(void)
 	struct wdt_conf config_wdt;
 	wdt_get_config_defaults(&config_wdt);
   config_wdt.clock_source         = GCLK_GENERATOR_3;
-	config_wdt.timeout_period       = WDT_PERIOD_8192CLK;
-	config_wdt.early_warning_period = WDT_PERIOD_4096CLK;
+	config_wdt.timeout_period       = WDT_PERIOD_16384CLK;
+	config_wdt.early_warning_period = WDT_PERIOD_16384CLK;
 	wdt_set_config(&config_wdt);
 
   wdt_register_callback(bms_wdt_early_warning_callback, WDT_CALLBACK_EARLY_WARNING);
